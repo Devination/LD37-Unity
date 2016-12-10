f4344d2c68a183f5b4d81589d88dd05d8ef72469
Devin Curry <devination3d@gmail.com>
2016-12-10 15:42:41 -0800

    Helicopter object that goes back and forth and can drop objects

---
+++ LD37/Assets/Scripts/FurnitureDropper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnitureDropper : MonoBehaviour {
+	[SerializeField]
+	Rigidbody2D[] furniture;
+	[SerializeField]
+	float speed = 2;
+
+	public Rigidbody2D selectedFurniture; //Assigned via FurnitureSelector.cs
+
+	Rigidbody2D body;
+
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+		body.velocity = Vector2.left * speed;
+	}
+
+	void Update() {
+		if (Input.anyKeyDown) {
+			Drop();
+		}
+
+		//Move back and forth
+		float posOnScreen = Camera.main.WorldToScreenPoint(transform.position).x;
+        if (posOnScreen < 0 || posOnScreen > Screen.width) {
+			body.velocity *= -1;
+        }
+	}
+
+	void Drop() {
+		if (selectedFurniture != null) {
+			Instantiate(selectedFurniture);
+		}
+		else {
+			//TODO: Should expose this in the on screen UI
+			Debug.Log("Select the furniture you want to drop first!");
+		}
+	}
+}
