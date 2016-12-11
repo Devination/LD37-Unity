877454f9d22a856b302eac463cbd58cc2090260c
DESKTOP-NM3FIKN\codym <codymalach@gmail.com>
2016-12-10 19:49:43 -0800

    Add client scene and UI Utils file.

---
+++ LD37/Assets/Scripts/ClientSceneManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientSceneManager : MonoBehaviour {
+	public Sprite ClientSprite;
+	public Sprite BlueprintSprite;
+	public TextAsset ClientText;
+
+	[SerializeField] GameObject BlueprintImage;
+	[SerializeField] GameObject ProceedButton;
+
+	private string[] clientDialogue;
+	private enum ClientSceneState {
+		Talking,
+		Blueprint,
+		Done
+	};
+	private ClientSceneState sceneState;
+	private GameObject dialogueObject;
+
+	void UpdateSceneState() {
+		sceneState = ClientSceneState.Blueprint;
+	}
+
+	// Use this for initialization
+	void Start () {
+		sceneState = ClientSceneState.Talking;
+		if ( ClientText != null ) {
+			clientDialogue = ( ClientText.text.Split( '\n' ) );
+			dialogueObject = GameObject.Find( "ClientDialogue" );
+			Text textComponent = dialogueObject.GetComponent<Text>();
+			Action callback = () => UpdateSceneState();
+			StartCoroutine( UIUtils.ScrollText( textComponent, clientDialogue, callback ) );
+		}
+
+		if ( ClientSprite != null ) {
+			GameObject client = GameObject.Find( "Client" );
+			Image clientImage = client.GetComponent<Image>();
+			clientImage.sprite = ClientSprite;
+		}
+
+		if ( BlueprintSprite != null ) {
+			GameObject blueprint = GameObject.Find( "Blueprint" );
+			Image blueprintImage = blueprint.GetComponent<Image>();
+			blueprintImage.sprite = BlueprintSprite;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( sceneState == ClientSceneState.Blueprint ) {
+			GameObject textBox = GameObject.Find( "TextBox" );
+			textBox.SetActive( false );
+			BlueprintImage.SetActive( true );
+			ProceedButton.SetActive( true );
+			sceneState = ClientSceneState.Done;
+		}
+	}
+}
---
+++ LD37/Assets/Scripts/Utils/UIUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class UIUtils {
+	public static IEnumerator ScrollText ( Text textComponent, string[] text, System.Action callback ) {
+		for ( int currentTextLine = 0; currentTextLine < text.Length; currentTextLine++ ) {
+			string currentString = text[currentTextLine];
+			textComponent.text = "";
+			for ( int characterIndex = 0; characterIndex < currentString.Length; characterIndex++ ) {
+				textComponent.text += currentString[characterIndex];
+				yield return new WaitForSeconds( 0.1f );
+			}
+		}
+
+		callback();
+	}
+}
