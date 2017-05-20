69eb5be35bef409c105558d2a54a2eb841292e97
Devin Curry <Devination3d@gmail.com>
2017-05-20 16:03:33 -0700

    Made furniture spawn in more juicy

---
+++ LD37/Assets/Scripts/RotationUnfreezer.cs
@@ -0,0 +1,14 @@
+﻿//Used to unfreeze the rotation after the furniture scaleIn anim. Unity's anim system couldn't keyframe this for some reason.
+using UnityEngine;
+
+public class RotationUnfreezer : MonoBehaviour {
+	Rigidbody2D body;
+
+	private void Start() {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	public void UnfreezeRotation() {
+		body.freezeRotation = false;
+	}
+}
