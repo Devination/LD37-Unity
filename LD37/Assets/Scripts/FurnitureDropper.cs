2c5f117a94f0f9656513c3d4519b9c8e3dfe626a
Devin Curry <Devination3d@gmail.com>
2017-05-20 15:11:22 -0700

    Furniture now hangs below drone after being selected

--- LD37/Assets/Scripts/FurnitureDropper.cs
+++ LD37/Assets/Scripts/FurnitureDropper.cs
@@ -9,8 +9,8 @@ public class FurnitureDropper : MonoBehaviour {
 	[SerializeField]
 	GameObject selectWarningText;
 
-	[HideInInspector]
-	public Rigidbody2D selectedFurniture; //Assigned via FurnitureSelector.cs
+	HingeJoint2D hinge;
+	Rigidbody2D selectedFurniture;
 
 	bool canDrop = true;
 	Rigidbody2D body;
@@ -18,46 +18,54 @@ public class FurnitureDropper : MonoBehaviour {
 
 	void Start() {
 		body = GetComponent<Rigidbody2D>();
+		hinge = GetComponent<HingeJoint2D>();
 		body.velocity = Vector2.left * speed;
 	}
 
 	void Update() {
-		if ( ( Input.GetMouseButtonDown(0) || Input.GetButtonDown( "Fire1" ) ) && canDrop) {
+		if ((Input.GetMouseButtonDown(0) || Input.GetButtonDown("Fire1")) && canDrop) {
 			Drop();
 		}
 
 		//Move back and forth
 		float posOnScreen = Camera.main.WorldToScreenPoint(transform.position).x;
-        if ((posOnScreen < 0 && body.velocity.x < 0) //On the left and moving left
+		if ((posOnScreen < 0 && body.velocity.x < 0) //On the left and moving left
 			|| (posOnScreen > Screen.width && body.velocity.x > 0)) { //On the right and moving right
 			body.velocity *= -1; //Reverse direction
-        }
+		}
 	}
 
 	void Drop() {
 		if (selectedFurniture != null) {
-			Rigidbody2D obj = (Rigidbody2D)Instantiate(selectedFurniture, transform.position, Quaternion.identity);
-			obj.velocity = body.velocity;
-			selectedFurniture = null;
-			obj.GetComponent<SpriteRenderer>().sortingOrder = itemsDropped;
+			selectedFurniture.velocity = body.velocity;
+			selectedFurniture.GetComponent<SpriteRenderer>().sortingOrder = itemsDropped;
 			itemsDropped++;
-        }
+			hinge.connectedBody = null;
+			selectedFurniture = null;
+		}
 		else {
 			selectWarningText.SetActive(true);
 			Invoke("TurnOffWarningText", 2);
-        }
+		}
 	}
 
 	void TurnOffWarningText() {
 		selectWarningText.SetActive(false);
 	}
 
+	//Called via Canvas Buttons, not .cs scripts
 	public void PickFurniture(int index) {
 		TurnOffWarningText();
-        selectedFurniture = furniture[index];
-    }
+		if (selectedFurniture != null) {
+			Destroy(selectedFurniture.gameObject);
+		}
+		Vector2 pos = transform.position;
+		pos.y -= 1;
+		selectedFurniture = (Rigidbody2D)Instantiate(furniture[index], pos, Quaternion.identity);
+		hinge.connectedBody = selectedFurniture;
+	}
 
 	public void SetDropEnabled(bool value) {
 		canDrop = value;
-    }
+	}
 }
